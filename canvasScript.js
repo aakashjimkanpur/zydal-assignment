479a0ebf600d2e519b7d1b3366f8e06f21c1d1ae
Aakash Gupta <aakash.jimkanpur@gmail.com>
2023-10-28 21:20:57 +0530

    update

--- canvasScript.js
+++ canvasScript.js
@@ -1,39 +1,36 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-ctx.font = "12px Arial";
+ctx.font = "12px sans-serif";
 ctx.fillStyle = "red";
 ctx.textAlign = "left";
-ctx.textBaseline = "middle";
-ctx.fillText("B", canvas.width / 2, canvas.height / 2);
+ctx.textBaseline = "left";
+ctx.fillText("A", canvas.width / 2, canvas.height / 2);
 
+function RGBAToHexA(r, g, b, a) {
+  r = r.toString(16);
+  g = g.toString(16);
+  b = b.toString(16);
+
+  if (r.length === 1) r = "00" + r;
+  if (g.length === 1) g = "00" + g;
+  if (b.length === 1) b = "00" + b;
+  if (r.length === 2) r = "0" + r;
+  if (g.length === 2) g = "0" + g;
+  if (b.length === 2) b = "0" + b;
+  return "0x" + r + g + b;
+}
 function downloadCanvas() {
-  const dataURL = canvas.toDataURL("image/png");
-  const data = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-  const fileData = hexEncode(data);
+  const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  let hex = "";
+  for (let i = 0; i < data.length; i = i + 4) {
+    if (i % 64 == 0) hex += "\n";
+    hex += RGBAToHexA(data[i], data[i] + 1, data[i] + 2, data[i] + 3) + ", ";
+  }
   const fileName = "canvas_data.txt";
-  const file = new Blob([fileData], { type: "text/plain" });
+  const file = new Blob([hex], { type: "text/plain" });
   if (window.navigator.msSaveOrOpenBlob) {
     window.navigator.msSaveOrOpenBlob(file, fileName);
   } else {
-    const a = document.createElement("a");
-    const url = URL.createObjectURL(file);
-    a.href = url;
-    a.download = fileName;
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(() => {
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    }, 0);
+    document.getElementById("dcanvas").href = URL.createObjectURL(file);
   }
-}
-
-function hexEncode(str) {
-  let hex, i;
-  let result = "";
-  for (i = 0; i < str.length; i++) {
-    hex = str.charCodeAt(i).toString(16);
-    result += ("000" + hex).slice(-4);
-  }
-  return result;
 }
